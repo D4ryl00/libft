4446eae62e04bd2a18275c1a8d603970b4c1a003
Remi BARBERO <rbarbero@student.42.fr>
2018-01-17 16:07:51 +0100

    support mb_cur_max

--- ft_wctomb.c
+++ ft_wctomb.c
@@ -6,7 +6,7 @@
 /*   By: rbarbero <rbarbero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/17 13:30:36 by rbarbero          #+#    #+#             */
-/*   Updated: 2018/01/17 15:05:10 by rbarbero         ###   ########.fr       */
+/*   Updated: 2018/01/17 16:03:27 by rbarbero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,20 @@ int	ft_wctomb(char *s, wchar_t wchar)
 	int	ret;
 
 	ret = 0;
-	if (s && wchar <= 0x7f && (ret = 1))
+	if (s && MB_CUR_MAX >= 1 && wchar <= 0x7f && (ret = 1))
 		s[0] = wchar;
-	else if (s && wchar <= 0x7ff && (ret = 2))
+	else if (s && MB_CUR_MAX >= 2 && wchar <= 0x7ff && (ret = 2))
 	{
 		s[0] = 0xc0 | wchar >> 6;
 		s[1] = 0x80 | (wchar & 0x3f);
 	}
-	else if (s && wchar <= 0xffff && (ret = 3))
+	else if (s && MB_CUR_MAX >= 3 && wchar <= 0xffff && (ret = 3))
 	{
 		s[0] = 0xe0 | wchar >> 12;
 		s[1] = 0x80 | ((wchar >> 6) & 0x3f);
 		s[2] = 0x80 | (wchar & 0x3f);
 	}
-	else if (s && wchar <= 0x1fffff && (ret = 4))
+	else if (s && MB_CUR_MAX >= 4 && wchar <= 0x1fffff && (ret = 4))
 	{
 		s[0] = 0xf0 | wchar >> 18;
 		s[1] = 0x80 | ((wchar >> 12) & 0x3f);
