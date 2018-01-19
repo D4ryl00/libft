f7b486ce7d0e6adddcd39389cd62627102f74c87
Remi BARBERO <rbarbero@student.42.fr>
2018-01-20 00:44:24 +0100

    fix bugs

--- ft_wctomb.c
+++ ft_wctomb.c
@@ -6,7 +6,7 @@
 /*   By: rbarbero <rbarbero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/17 13:30:36 by rbarbero          #+#    #+#             */
-/*   Updated: 2018/01/17 16:03:27 by rbarbero         ###   ########.fr       */
+/*   Updated: 2018/01/19 17:11:06 by rbarbero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int	ft_wctomb(char *s, wchar_t wchar)
 	ret = 0;
 	if (s && MB_CUR_MAX >= 1 && wchar <= 0x7f && (ret = 1))
 		s[0] = wchar;
+	else if (s && MB_CUR_MAX == 1 && wchar <= 0xff && (ret = 1))
+		s[0] = wchar;
 	else if (s && MB_CUR_MAX >= 2 && wchar <= 0x7ff && (ret = 2))
 	{
 		s[0] = 0xc0 | wchar >> 6;
@@ -32,7 +34,7 @@ int	ft_wctomb(char *s, wchar_t wchar)
 		s[1] = 0x80 | ((wchar >> 6) & 0x3f);
 		s[2] = 0x80 | (wchar & 0x3f);
 	}
-	else if (s && MB_CUR_MAX >= 4 && wchar <= 0x1fffff && (ret = 4))
+	else if (s && MB_CUR_MAX >= 4 && wchar <= 0x10ffff && (ret = 4))
 	{
 		s[0] = 0xf0 | wchar >> 18;
 		s[1] = 0x80 | ((wchar >> 12) & 0x3f);
