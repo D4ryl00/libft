0b3d96d0c15523786b37544d9a3cdf1f3d0e6966
Remi BARBERO <rbarbero@student.42.fr>
2018-03-24 09:35:32 +0100

    mofify ft_lstmap to accept to ignore selected nodes

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,14 +6,14 @@
 /*   By: rbarbero <rbarbero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/16 16:20:23 by rbarbero          #+#    #+#             */
-/*   Updated: 2017/11/16 22:00:57 by rbarbero         ###   ########.fr       */
+/*   Updated: 2018/03/24 09:34:17 by rbarbero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-static void	free_memory(t_list *lst)
+static t_list	*free_memory(t_list *lst)
 {
 	t_list	*next;
 
@@ -24,9 +24,10 @@ static void	free_memory(t_list *lst)
 		free(lst);
 		lst = next;
 	}
+	return (NULL);
 }
 
-t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
 	t_list	*new;
 	t_list	*p;
@@ -39,17 +40,17 @@ t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 	while (lst)
 	{
 		tmp = f(lst);
-		if (!(p = ft_lstnew(tmp->content, tmp->content_size)))
+		if (tmp)
 		{
-			free_memory(new);
-			return (NULL);
+			if (!(p = ft_lstnew(tmp->content, tmp->content_size)))
+				return (free_memory(new));
+			if (is_first)
+				new = p;
+			else
+				prev->next = p;
+			is_first = 0;
+			prev = p;
 		}
-		if (is_first)
-			new = p;
-		else
-			prev->next = p;
-		is_first = 0;
-		prev = p;
 		lst = lst->next;
 	}
 	return (new);
