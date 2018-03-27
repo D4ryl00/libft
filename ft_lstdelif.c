91d5a3da83b87aa4ce124c11ce981286c56549ae
Remi BARBERO <rbarbero@student.42.fr>
2018-03-27 09:34:14 +0200

    bug fix in ft_lstdelif

--- ft_lstdelif.c
+++ ft_lstdelif.c
@@ -6,21 +6,43 @@
 /*   By: rbarbero <rbarbero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/26 07:28:24 by rbarbero          #+#    #+#             */
-/*   Updated: 2018/03/26 17:49:49 by rbarbero         ###   ########.fr       */
+/*   Updated: 2018/03/27 08:51:13 by rbarbero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelif(t_list **list, int (*test)(t_list *)
+void static	delete_node(t_list **list, t_list **previous, t_list **current
 		, void (*del)(void *, size_t))
 {
-	t_list	*next;
-	while (*list)
+	if (*current == *list)
 	{
-		next = (*list)->next;
-		if (test(*list))
-			ft_lstdelnode(list, *list, del);
-		*list = next;
+		ft_lstdelnode(list, *current, del);
+		*current = *list;
+	}
+	else
+	{
+		ft_lstdelnode(previous, *current, del);
+		*current = (*previous)->next;
+	}
+}
+
+void		ft_lstdelif(t_list **list, int (*test)(t_list *)
+		, void (*del)(void *, size_t))
+{
+	t_list	*previous;
+	t_list	*current;
+
+	previous = NULL;
+	current = *list;
+	while (current)
+	{
+		if (test(current))
+			delete_node(list, &previous, &current, del);
+		else
+		{
+			previous = current;
+			current = current->next;
+		}
 	}
 }
