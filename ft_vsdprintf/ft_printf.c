6d3ae985da57327c89869e6a4150dd14a3a510c2
d4ryl00 <d4ryl00@gmail.com>
2018-04-02 09:56:17 +0200

    fix ft_printf

--- ft_vsdprintf/ft_printf.c
+++ ft_vsdprintf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: rbarbero <rbarbero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/01 16:36:17 by rbarbero          #+#    #+#             */
-/*   Updated: 2018/04/01 23:42:26 by rbarbero         ###   ########.fr       */
+/*   Updated: 2018/04/02 09:54:29 by rbarbero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_printf(const char *format, ...)
 	va_list	va_args;
 
 	va_start(va_args, format);
-	ret = ft_dprintf(1, format, va_args);
+	ret = ft_vsdprintf(NULL, 1, format, va_args);
 	va_end(va_args);
 	return (ret);
 }
