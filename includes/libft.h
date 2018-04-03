c43f5b8eef3ac8d3fd12d3abd73e3efd850bfaed
d4ryl00 <d4ryl00@gmail.com>
2018-04-03 11:25:42 +0200

    add ft_lstselect

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: rbarbero <rbarbero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 12:13:41 by rbarbero          #+#    #+#             */
-/*   Updated: 2018/04/03 10:28:07 by rbarbero         ###   ########.fr       */
+/*   Updated: 2018/04/03 11:24:29 by rbarbero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,6 +109,8 @@ void			ft_lstdelnode(t_list **previous, t_list *node
 		, void (*del)(void *, size_t));
 void			ft_lstdelif(t_list **list, void *data
 		, int (*test)(t_list *, void *), void (*del)(void *, size_t));
+t_list			*ft_lstselect(t_list *list, void *data
+		, int (*test)(t_list *, void *));
 unsigned int	ft_char_encode_utf8(unsigned int c);
 unsigned int	ft_char_decode_utf8(unsigned int c);
 int				ft_is_valid_unicode(wint_t wchar);
