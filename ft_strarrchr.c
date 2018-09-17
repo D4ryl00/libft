e8a8babfc2e14054808009df049b2ca6c4052107
Remi BARBERO <rbarbero@student.42.fr>
2018-09-17 16:09:31 +0200

    add ft_strarrchr

---
+++ ft_strarrchr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strarrchr.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbarbero <rbarbero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/17 16:01:14 by rbarbero          #+#    #+#             */
+/*   Updated: 2018/09/17 16:07:42 by rbarbero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** Return index of str if it's is in array. Else return -1.
+*/
+
+int	ft_strarrchr(char *str, char **arr)
+{
+	int	i;
+
+	i = -1;
+	if (arr)
+	{
+		while (arr[++i])
+		{
+			if (!ft_strcmp(arr[i], str))
+				return (i);
+		}
+	}
+	return (-1);
+}
