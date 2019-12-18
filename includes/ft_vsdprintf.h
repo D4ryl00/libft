d3911b6cd70798e69d876205632e01fc2132031c
rbarbero <rbarbero@student.42.fr>
2019-12-18 13:14:37 +0100

    norme ok

--- includes/ft_vsdprintf.h
+++ includes/ft_vsdprintf.h
@@ -6,7 +6,7 @@
 /*   By: rbarbero <rbarbero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/01 17:05:51 by rbarbero          #+#    #+#             */
-/*   Updated: 2019/12/18 12:13:16 by rbarbero         ###   ########.fr       */
+/*   Updated: 2019/12/18 13:14:02 by rbarbero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,20 +30,20 @@ typedef struct	s_seq
 
 typedef struct	s_conv
 {
-	int		position;
-	UCHAR	hash;
-	UCHAR	zero;
-	UCHAR	minus;
-	UCHAR	space;
-	UCHAR	plus;
-	UCHAR	quote;
-	int		*width;
-	UCHAR	dot;
-	int		*precision;
-	char	modifier[3];
-	char	specifier[2];
-	void	*data;
-	t_seq	*(*fconv)(struct s_conv *conv, intmax_t n);
+	int				position;
+	unsigned char	hash;
+	unsigned char	zero;
+	unsigned char	minus;
+	unsigned char	space;
+	unsigned char	plus;
+	unsigned char	quote;
+	int				*width;
+	unsigned char	dot;
+	int				*precision;
+	char			modifier[3];
+	char			specifier[2];
+	void			*data;
+	t_seq			*(*fconv)(struct s_conv *conv, intmax_t n);
 }				t_conv;
 
 typedef struct	s_arg
@@ -67,25 +67,25 @@ intmax_t		ft_printf(const char *format, ...);
 void			init_convs(t_conv *convs, int nbr);
 t_seq			*init_seq(char *str, size_t len);
 t_seq			*print_conv(t_conv *conv, intmax_t n);
-UCHAR			parse_format(t_conv *convs, const char *format
+unsigned char	parse_format(t_conv *convs, const char *format
 		, va_list va_args);
 void			parse_position_arg(t_conv *conv, const char **fmt, int *arg_i);
 void			parse_flags(t_conv *conv, const char **fmt);
-UCHAR			parse_width(t_conv *conv, const char **fmt, t_list **args
+unsigned char	parse_width(t_conv *conv, const char **fmt, t_list **args
 		, int *arg_i);
-UCHAR			parse_precision(t_conv *conv, const char **fmt, t_list **args
+unsigned char	parse_precision(t_conv *conv, const char **fmt, t_list **args
 		, int *arg_i);
 void			parse_modifier(t_conv *conv, const char **fmt);
-UCHAR			parse_specifier(t_conv *conv, const char **fmt);
+unsigned char	parse_specifier(t_conv *conv, const char **fmt);
 char			get_specifier(const char *str);
 void			get_modifier(char *modifier, const char *str);
-UCHAR			new_arg(t_list **args, int pos, const char *type, void **data);
+unsigned char	new_arg(t_list **args, int pos, const char *type, void **data);
 void			take_args(t_list **args, va_list va_args);
 void			arg_del(void *content, size_t content_size);
-UCHAR			take_int_args(t_list *args, va_list va_args);
-UCHAR			take_char_args(t_list *args, va_list va_args);
-UCHAR			take_str_args(t_list *args, va_list va_args);
-UCHAR			take_n_args(t_list *args, va_list va_args);
+unsigned char	take_int_args(t_list *args, va_list va_args);
+unsigned char	take_char_args(t_list *args, va_list va_args);
+unsigned char	take_str_args(t_list *args, va_list va_args);
+unsigned char	take_n_args(t_list *args, va_list va_args);
 
 t_seq			*int_conv(t_conv *conv, intmax_t n);
 char			*int_mod_space(char **str);
@@ -104,8 +104,8 @@ t_seq			*str_conv(t_conv *conv, intmax_t n);
 t_seq			*percent_conv(t_conv *conv, intmax_t n);
 t_seq			*n_conv(t_conv *conv, intmax_t n);
 t_seq			*default_conv(t_conv *conv, intmax_t n);
-UCHAR			buf_add_str(t_buffer *buf, char *str);
-UCHAR			buf_add_seq(t_buffer *buf, t_seq *seq);
-UCHAR			buf_print(char **str, int fd, t_buffer *buf);
-UCHAR			print_color(t_buffer *buf, const char **fmt);
+unsigned char	buf_add_str(t_buffer *buf, char *str);
+unsigned char	buf_add_seq(t_buffer *buf, t_seq *seq);
+unsigned char	buf_print(char **str, int fd, t_buffer *buf);
+unsigned char	print_color(t_buffer *buf, const char **fmt);
 #endif
